3304f26ab051c53da2cb7b08306084776bda26b9
Joao Luis Rosa de Moura <Joao.Moura@zaffari.com.br>
2024-12-02 16:43:49 -0300

    Trino funcional

--- init_trino/create_schemas.sql
+++ init_trino/create_schemas.sql
@@ -1,5 +1,48 @@
-CREATE SCHEMA IF NOT EXISTS minio.bruto 
-WITH (location = 's3a://bruto/');
--- create schema if not exists minio.refinado with (location = 's3a://refinado/');
+create schema if not exists minio.bruto with (location = 's3a://bruto/');
+create schema if not exists minio.refinado with (location = 's3a://refinado/');
 
         
+create table if not exists 
+	minio.bruto.musicas (
+		Musica VARCHAR,
+		Artista VARCHAR,
+		Gravadora VARCHAR
+	) 
+with (
+	    external_location = 's3a://bruto/',
+	    format = 'CSV',
+	    skip_header_line_count=1,
+		csv_separator = ';'
+    )
+;
+
+create table if not exists 
+	minio.refinado.track (
+		name VARCHAR,
+		album VARCHAR,
+		artists VARCHAR,
+		release_date VARCHAR
+	) 
+with (
+	    external_location = 's3a://refinado/',
+	    format = 'CSV',
+	    skip_header_line_count=1,
+		csv_separator = ';'
+    )
+;
+
+create table if not exists 
+	minio.refinado.artists (
+		name VARCHAR,
+		genres VARCHAR,
+		popularity VARCHAR,
+		followers VARCHAR
+	) 
+with (
+	    external_location = 's3a://refinado/',
+	    format = 'CSV',
+	    skip_header_line_count=1,
+		csv_separator = ';'
+    )
+;
+    
